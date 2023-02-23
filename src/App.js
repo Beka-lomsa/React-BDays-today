39a4eedb1bc12374e8f710448b611d1dd076fce9
Beka <past.present.future.end@gmail.com>
2023-02-24 01:13:25 +0400

    init

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import List from './List'
+import {Data} from './Data'
+
+
+
+function App() {
+const [people, setPeople] = useState(Data)
+
+  return (
+  <main>
+    <section className='container'>
+      <h3>{people.length} birthdays today</h3>
+      <List people={people}/>
+      <button onClick={()=> setPeople([])}
+      >clear all</button>
+      
+    </section>
+  </main>
+  )
+}
+
+export default App;
